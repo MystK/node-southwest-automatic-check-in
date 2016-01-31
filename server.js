36c1e1e5a0647fefddcb9f6c55f4875e608d923f
Antoine Pham <its@phamap.net>
2016-01-30 17:09:50 -0800

    syntac refactor

--- server.js
+++ server.js
@@ -4,49 +4,50 @@ import koaRouter from 'koa-router'
 import request from 'request'
 import _ from 'lodash'
 import { CronJob } from 'cron'
+import cors from 'kcors'
+import bodyParser from 'koa-bodyparser'
 var jobs = []
-// CronJob
 const server = new Koa();
 const router = koaRouter();
 const onerror = err => {
   console.error(err.stack)
 }
 
-import cors from 'kcors'
-import bodyParser from 'koa-bodyparser'
-server.use(bodyParser())
-
-server.use(async(ctx, next) => {
-  console.log(ctx.url)
-  await next()
-})
-
-const parseSetCookie = input => {
-  if (!input) return ''
-  let output = []
-  input.forEach((e, i) => {
-    output[i] = e.split(';')[0]
+server
+  .use(bodyParser())
+  .use(async(ctx, next) => {
+    console.log(ctx.url)
+    await next()
   })
-  return output.join('; ')
-}
-const defaultRequestOptions = {
-  headers: {
-    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2605.0 Safari/537.36',
-    'Accept': 'text/html',
-    'Accept-Encoding': 'deflate',
-    'Accept-Language': 'en-US,en;q=0.8'
-  }
-}
+
 const SWRequest = (options, redirectCount = 0) => new Promise((resolve, reject) => (async f => {
-  let post = await new Promise((resolve, reject) => {
-    request.post(Object.assign(defaultRequestOptions, options), (err, res, body) => { if (err) reject(err); else resolve([res, body]) })
+  const dRO = { // defaultRequestOptions
+    headers: {
+      'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2605.0 Safari/537.36',
+      'Accept': 'text/html',
+      'Accept-Encoding': 'deflate',
+      'Accept-Language': 'en-US,en;q=0.8'
+    }
+  }
+  const parseSetCookie = input => {
+    if (!input) return ''
+    let output = []
+    input.forEach((e, i) => {
+      output[i] = e.split(';')[0]
+    })
+    return output.join('; ')
+  }
+  let [postRes, postBody] = await new Promise((resolve, reject) => {
+    request.post(_.assign(dRO, options), (err, res, body) => {
+      if (err) reject(err)
+      else resolve([res, body])
+    })
   })
-  let [postRes, postBody] = post
   let postCookie = parseSetCookie(postRes.headers['set-cookie'])
   if (postRes.statusCode === 302) {
     let redirect = await new Promise((resolve, reject) => {
       let [url, setCookie] = [postRes.headers.location, postRes.headers['set-cookie']]
-      request.get(Object.assign(defaultRequestOptions, {
+      request.get(_.assign(dRO, {
         url: url,
         headers: { Cookie: postCookie }
       }), (err, res, body) => { if (err) reject(err); else resolve([res, body]) })
